84788c90db83b697a90f90acedce72f2db433ef4
ajith <ajithchowdary96@gmail.com>
2020-06-07 18:45:55 -0400

    Lazy loading implemented

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,11 +4,16 @@ import { StudentComponent } from './student/student.component';
 import { ParentComponent } from './parent/parent.component';
 import { HomeComponent } from './home/home.component';
 
+
 const routes: Routes = [
     {path : '', redirectTo : '/home', pathMatch : 'full'},
     { path: 'student', component: StudentComponent }, 
     {path : 'parent', component: ParentComponent},
-    {path : 'home', component : HomeComponent}
+    {path : 'home', component : HomeComponent},
+    { path: 'staff', loadChildren: () => import('src/app/staff/staff.module').then(m => m.StaffModule) }
+
+
+    //  { path: 'staff', loadChildren: () => import('src/app/staff/staff.module').then(m => m.StaffModule) }
 ];
 
 
