1b7a6b006477fbf3dcfcec77f80e0375f890b179
ajith <ajithchowdary96@gmail.com>
2020-06-12 10:28:01 -0400

    changes

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AppService } from '../app.service';
 
 @Component({
   selector: 'app-footer',
@@ -6,16 +7,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./footer.component.scss']
 })
 export class FooterComponent implements OnInit {
-  name : string = 'ajith';
+  id : string;
+  name : string;
+  employee : any;
+  // studentIdandName: any;
 
-  constructor() { }
+  constructor(private appService : AppService) { }
 
   ngOnInit(): void {
   }
 
 
   checkService(){
-      console.log(this.name);
+      // console.log(this.name);
+      // this.appService.getEmployeeById(this.id).subscribe((response)=>{
+      //   this.employee = response;
+      // });
+      
+      
+  }
+
+  getStudentDetails(){
+    // this.appService.getStudentData(this.id, this.name)
+
   }
 
 }
